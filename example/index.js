178a0b3014e45c286430f6ed8de770c15ca2fd26
Otter <otc@otterthecat.com>
2015-07-01 22:46:15 -0500

    offshore.js now returns a transfrom stream so it can be piped by the user

--- example/index.js
+++ example/index.js
@@ -1,3 +1,11 @@
+var fs = require('fs');
 var offshore = require('../src/offshore');
 
-offshore('source.js', 'done.js');
+ var readStream = fs.createReadStream('source.js', {
+  'encoding': 'utf8'
+});
+var writeStream = fs.createWriteStream('done.js');
+
+readStream
+  .pipe(offshore)
+  .pipe(writeStream);
--- src/offshore.js
+++ src/offshore.js
@@ -1,5 +1,4 @@
 'use strict';
-var fs = require('fs');
 var Transform = require('stream').Transform;
 
 var ifCheckRegX = /[^if\(](\w+)(?=\))/g;
@@ -10,16 +9,9 @@ Transform.prototype._transform = function(data, encoding, callback){
   newData = newData.replace(parseIntRegX, 'parseInt($& + "")');
   callback(null, newData);
 };
+
 var offshorify = new Transform({
-  'decodeStrings': false
+  "decodeStrings": false
 });
 
-module.exports = function(source, destination){
-
-  var readStream = fs.createReadStream(source, {
-    'encoding': 'utf8'
-  });
-  var writeStream = fs.createWriteStream(destination);
-
-  readStream.pipe(offshorify).pipe(writeStream);
-};
+module.exports = offshorify;
